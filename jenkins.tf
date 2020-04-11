c6c755f4d1c4d2861455d5567ed7761ab53d4463
shegoj <shegoj@yahoo.com>
2020-04-11 21:03:06 +0100

    adding first file

---
+++ jenkins.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket = "shegoj-tfstate"
+    key    = "terraform_state_via_jenkins"
+    region = "eu-west-1"
+  }
+}
+
+resource "aws_instance" "via_jenkins"  {
+
+    ami           = "ami-06ce3edf0cff21f07"
+    instance_type = "t2.micro"
+    count         = 3
+    key_name      = "devopsmodule"
+
+    tags = {
+        Name ="For_Jenkins"
+    }
+}
+
+provider "aws" {
+    region = "eu-west-1"
+}
